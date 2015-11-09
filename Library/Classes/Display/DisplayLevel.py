12a15b35b552a205d619593d157818df69a6ac27
Nathan Veillon <nathan.veillon@gmail.com>
2015-11-09 14:32:48 -0700

    Include Selector For Clicking on different Type of levels.

--- Library/Classes/Display/DisplayLevel.py
+++ Library/Classes/Display/DisplayLevel.py
@@ -2,6 +2,7 @@
 # 
 # File Contributors
 #     Nathan Veillon
+#     Josh Rosen
 #
 
 from View.Levels.ImportLevels import *
@@ -14,17 +15,30 @@ class DisplayLevel:
         self.level = eval(level_file_name)()
         self.letter_map = self.level.letter_map
         self.tile_map = TileMap(self.letter_map)
+        # self.level_menu = LevelMenu()
 
     def display_tile_map(self):
         self.tile_map.display_tile_map()
 
     def window_clicked(self):
-        #for event in pygame.event.get(MOUSEBUTTONUP)
-        #if (mouse position within tile map)
-        #   tilemap.clicked(mouse position)
-        #elif (mouse position within tile)
-        #   levelmenu.clicked(mouse position)
-        return None
+        for event in pygame.event.get(MOUSEBUTTONUP):
+            mouse_position = event.pos
+            if self.mouse_position_in_tile_map(mouse_position):
+                self.tile_map.clicked(mouse_position)
+            elif self.mouse_position_in_level_menu(mouse_position):
+                self.level_menu.clicked(mouse_position)
+
+    def mouse_position_in_tile_map(self,mouse_position):
+        x,y = mouse_position
+        if(x <= self.tile_map.map_size):
+            return True
+        return False
+
+    def mouse_position_in_level_menu(self,mouse_position):
+        x,y = mouse_position
+        if(x >= self.tile_map.map_size):
+            return True
+        return False
 
 
 
--- Library/Classes/Tiles/Terrain.py
+++ Library/Classes/Tiles/Terrain.py
@@ -11,6 +11,7 @@ class Terrain(BaseTile):
     def __init__(self,tile_size):
         BaseTile.__init__(self,tile_size,'Library/Assets/Tiles/Terrain.png')
         self.type = 'Terrain'
+        self.Tower = None
         self.can_place_tower = True
 
 
