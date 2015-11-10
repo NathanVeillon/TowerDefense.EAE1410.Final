2c0f1193003da7e5f7473fb0992c613a3dc909d8
Joshua Rosen <joshrosen117@yahoo.com>
2015-11-10 15:32:05 -0700

    Working on StartMenu

--- View/StartMenu.py
+++ View/StartMenu.py
@@ -4,13 +4,16 @@
 #     
 # 
 # Purpose:
-# 
+#
+import pygame
+from pygame.locals import *
 class StartMenu():
 
     def __init__(self):
         self.window = pygame.display.get_surface()
-        self.menu_base = pygame.Surface((self.map_size, self.map_size), flags=SRCALPHA, depth=32)
+        self.menu_base = pygame.Surface((self.window.get_width(), self.window.get_height()), flags=SRCALPHA, depth=32)
+
     #player clicked on the start menu
     def clicked(self, mouse_pos):
-
+        return None 
 
