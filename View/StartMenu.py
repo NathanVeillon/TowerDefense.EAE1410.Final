fd9e90862a67982b6304201ac523cae68ec913c7
Joshua Rosen <joshrosen117@yahoo.com>
2015-11-10 18:15:33 -0700

    Testing my GIT

--- View/StartMenu.py
+++ View/StartMenu.py
@@ -14,7 +14,7 @@ class StartMenu():
 
     def __init__(self):
         self.window = pygame.display.get_surface()
-        self.map_size = self.window.get_height() #The size of the (square) tile map
+        self.map_size = self.window.get_height() #The size of the tile map (the tile map is a square - this value is both the width and the height)
         self.menu_base = pygame.Surface((self.window.get_width() - self.map_size, self.window.get_height()), flags=SRCALPHA, depth=32)
         self.base_tower_button = SimpleButton(50, 150, (100, 120, 90), (0, 0, 0), "Push Me", 25, self.menu_base, (10, 20))
 
