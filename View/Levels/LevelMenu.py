01e0b1fd2ae48be33511c4d1649ce35f0d455f93
Joshua Rosen <joshrosen117@yahoo.com>
2015-11-10 20:22:27 -0700

    Added some comments, fixed naming conventions

--- View/Levels/LevelMenu.py
+++ View/Levels/LevelMenu.py
@@ -3,9 +3,9 @@
 # File Contributors
 #     Joshua Rosen
 #
-# Purpose: Panel on the right hand side of the window for containing
+# Purpose:
+# Panel on the right hand side of the window for containing
 #   the buttons for the player to purchase and place towers.
-#
 
 import pygame, sys
 from pygame.locals import *
@@ -22,13 +22,12 @@ class LevelMenu():
         #The menu_base is the panel surface that contains all of the tower buttons
         self.menu_base = pygame.Surface((self.window.get_width() - self.map_size, self.window.get_height()), flags=SRCALPHA, depth=32)
 
-        self.base_tower_button = SimpleButton(50, 150, (100, 120, 90), (0, 0, 0), "Push Me", 25, self.menu_base, (10, 20))
+        self.base_tower_button = SimpleButton(50, 150, (100, 120, 90), (0, 0, 0), "Base Tower", 25, self.menu_base, (10, 20))
 
     def display_start_menu(self):
-        self.base_tower_button.displayBut()
+        self.base_tower_button.display_button()
         self.window.blit(self.menu_base,(self.map_size,0))
 
-    #Player clicked on the start menu
     def clicked(self, mouse_pos):
         #Subtract the width of the tile_map (map_size) from mouse_pos to get an x-coordinate relative to the start menu panel
         level_menu_mouse_pos = (mouse_pos[0] - self.map_size, mouse_pos[1])
--- View/SimpleButton.py
+++ View/SimpleButton.py
@@ -1,70 +1,60 @@
-# simpleButtonClass003.py
+# SimpleButton.py
+#
+# File Contributors
+#     Joshua Rosen
+#
+# Purpose:
+# Simple button class for the LevelMenu
 
 import pygame
 from pygame.locals import *
 
 class SimpleButton:
 
-    # class that creates button objects
+    def __init__(self, height, width, color, text_color, label, text_size, surf, position):
+        self.surf = surf
+        self.pos = position
+        self.button_color  = color
 
-    def __init__(self, height, width, color, textColor, label, textSize, surf, position):
-
-        # define some values
-
-        self.SURF = surf
-        self.POS = position
-        self.BUTCOLOR  = color
-        self.HIGHLIGHTCOLOR = (color[0] + ((255 - color[0])//3),
+        self.highlight_color = (color[0] + ((255 - color[0])//3),
                                color[1] + ((255 - color[1])//3),
                                color[2] + ((255 - color[2])//3))
 
-        self.TEXTCOLOR = textColor
-        self.TEXTSIZE = textSize
-
-        self.HEIGHT   = height
-        self.WIDTH  = width
-        self.RADIUS   = self.HEIGHT//2
-        #THEIGHT  = int(self.HEIGHT * .72)
+        self.height = height
+        self.width = width
+        self.radius = self.height//2
 
         self.ACTIVE = True
         self.HILIGHTED = False
 
-        self.BUTFONT = pygame.font.SysFont("Sylfaen", self.TEXTSIZE)
-        # Render a Text Surface
-        self.TEXTSURF = self.BUTFONT.render(label, True, textColor, None)
+        button_font = pygame.font.SysFont("Sylfaen", text_size)
+        self.text_surf = button_font.render(label, True, text_color, None)
 
-        w, h   = self.TEXTSURF.get_size()
+        w, h = self.text_surf.get_size()
 
-        self.XPOS   = (self.WIDTH - w)//2
-        self.YPOS   = int((self.HEIGHT - h)//2)
+        self.XPOS = (self.width - w)//2
+        self.YPOS = int((self.height - h)//2)
 
-        self.BUTTONSURF = pygame.Surface((self.WIDTH, self.HEIGHT), flags=SRCALPHA, depth=32)
+        self.BUTTONSURF = pygame.Surface((self.width, self.height), flags=SRCALPHA, depth=32)
         self.BUTTONSURF.fill((0, 0, 0, 0))
 
     def buttonBG(self, color):
+        #Draws a square with rounded corners
+        pygame.draw.circle(self.BUTTONSURF, color, (self.radius, self.radius), self.radius)
+        pygame.draw.circle(self.BUTTONSURF, color, (self.width - self.radius, self.radius), self.radius)
 
-        # create square with rounded corners
+        pygame.draw.rect(self.BUTTONSURF, color,  Rect((self.radius, 0), (self.width - 2 * self.radius, self.height)))
 
-        pygame.draw.circle(self.BUTTONSURF, color, (self.RADIUS, self.RADIUS),
-                           self.RADIUS)
-        pygame.draw.circle(self.BUTTONSURF, color,
-                           (self.WIDTH - self.RADIUS, self.RADIUS), self.RADIUS)
+    def __buttonText(self):
+        self.BUTTONSURF.blit(self.text_surf, (self.XPOS, self.YPOS))
 
-        pygame.draw.rect(self.BUTTONSURF, color,  Rect((self.RADIUS, 0), (self.WIDTH - 2 * self.RADIUS, self.HEIGHT)))
-
-    def buttonText(self):
-
-        # Draw Text
-        self.BUTTONSURF.blit(self.TEXTSURF, (self.XPOS, self.YPOS))
-
+    #Returns true if the user clicked within the button bounds
     def clicked(self, MOUSEXY):
-        yesORno = False
-        P1 = self.POS
-        P2 = (P1[0] + self.WIDTH, P1[1] + self.HEIGHT)
-        yesORno = (self.ACTIVE and P1[0] <= MOUSEXY[0] <= P2[0] and
-                   P1[1] <= MOUSEXY[1] <= P2[1])
+        P1 = self.pos
+        P2 = (P1[0] + self.width, P1[1] + self.height)
+        within_bounds = (self.ACTIVE and P1[0] <= MOUSEXY[0] <= P2[0] and P1[1] <= MOUSEXY[1] <= P2[1])
 
-        return yesORno
+        return within_bounds
 
     def Active(self):
         self.ACTIVE = True
@@ -74,18 +64,18 @@ class SimpleButton:
         self.ACTIVE = False
         return False
 
-
-    def displayBut(self):
+    #Blits button onto surface
+    def display_button(self):
 
         if self.ACTIVE:
             if self.HILIGHTED:
-                self.buttonBG(self.HIGHLIGHTCOLOR)
-                self.buttonText()
-                self.SURF.blit(self.BUTTONSURF, self.POS)
+                self.buttonBG(self.highlight_color)
+                self.__buttonText()
+                self.surf.blit(self.BUTTONSURF, self.pos)
             else:
-                self.buttonBG(self.BUTCOLOR)
-                self.buttonText()
-                self.SURF.blit(self.BUTTONSURF, self.POS)
+                self.buttonBG(self.button_color)
+                self.__buttonText()
+                self.surf.blit(self.BUTTONSURF, self.pos)
 
 
 
