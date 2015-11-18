842c36dd0500f988cf8985cb17eaa8a14804fd04
Joshua Rosen <joshrosen117@yahoo.com>
2015-11-18 15:30:37 -0700

    Added BaseBullet.png Added empty BaseBullet.py class

---
+++ Library/Classes/Bullets/BaseBullet.py
@@ -0,0 +1,14 @@
+# BaseBullet.py
+#
+# File Contributors
+#     Joshua Rosen
+#
+# Purpose:
+# BaseBullet class, used for the bullets that the towers shoot.
+#   If, in the future, we decide to have multiple towers with multiple types
+#   of bullets, this class could be inherited.
+
+class BaseBullet():
+
+    def __init__(self):
+        pass
