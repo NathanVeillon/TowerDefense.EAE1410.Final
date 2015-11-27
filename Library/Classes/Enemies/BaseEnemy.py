86fab6aa86742e633e6757fba5d42f3d67a358eb
Calatrast <penguinguy100@yahoo.com>
2015-11-27 16:32:51 -0700

    Made progress on BaseEnemy and EnemyWave. Both still need work.

--- Library/Classes/Enemies/BaseEnemy.py
+++ Library/Classes/Enemies/BaseEnemy.py
@@ -4,24 +4,66 @@
 #       David Mirabile
 # 
 # Purpose:
-# Generates an individual enemy.
+# Holds basic information about health and displaying of an enemy.
 
 import pygame, math
+from Library.Classes.Tiles.Path import *
+from Library.Classes.Bullets.Vector import *
 from random import randint
 
+## Implementation notice:
+## This class is an individual enemy.
+## This class will be managed by the EnemyWave class
 
+## This class is unfinished.
 class newEnemy:
 
-    def __init__(self, pos, surf, speed, health):
-        self.POS = pos
+    def __init__(self, pos, surf, speed, health, tileSize, image):
+        self.pos = pos
         self.SURF = surf
         self.SPEED = speed
-        self.HEALTH = health
+        self.TILESIZE = tileSize
+        self.health = health
+        self.change = self.SPEED
+        self.feed = False
 
+        self.IMAGE = image
 
+    ## Checks to see if the enemy has reached the next tile, and then...
+    ## if the enemy has reached the next tile, requests new info
+    ## BUGGED NEEDS FIXING
+    ## BUGGED NEEDS FIXING
+    ## BUGGED NEEDS FIXING
+    def __moveEnemy(self):
+        if (self.pos % self.TILESIZE < self.SPEED):
+            self.change = self.POS % self.TILESIZE
+            self.feed = True
+
+        self.pos += self.change
+        self.change = self.SPEED
+
+    ## this function will be called by the Enemy Wave class to see if an enemy needs new information.
+    ## if it returns true, the enemy wave will feed it more info.
+    def checkFeed(self):
+        if (self.feed):
+            return True
+
+        return False
+
+    def updateEnemy(self, speed):
+        self.SPEED = speed
     
-    def damage_enemy(self, damage):
-        self.HEALTH -= damage
+    def getPosition(self):
+        return self.POS
 
+    ## Damages the enemy.
+    def damage_enemy(self, damage):
+        self.health -= damage
+    
+    ## displays the enemy.
     def disp_enemy(self):
-        pass
+        __moveEnemy()
+        self.SURF.blit(IMAGE, pos)
+
+
+
--- Library/Classes/Enemies/EnemyWave.py
+++ Library/Classes/Enemies/EnemyWave.py
@@ -1,7 +1,50 @@
-#EnemyWave.py
+﻿#EnemyWave.py
 # 
 # File Contributors
-#
+#       David Mirabile
 # 
 # Purpose:
-# 
+# Manage and construct waves of enemies.
+
+
+from Library.Classes.Enemies.BaseEnemy import *
+
+
+## implementation notice:
+## This class is designed to manage enemies, and generate waves of enemies.
+## This class should be used when dealing with enemies
+
+## This class is unfinished
+class enemyWave():
+
+    def __init__(self, tileMap, tileSize, surf):
+        self.enemyList = []
+        self.TILEMAP = tileMap
+        self.TILESIZE = tileSize
+        self.SURF = surf
+
+    ## passes the enemy list to a function that requests it
+    def getEnemyList(self):
+        return enemyList
+
+    ## generates a new wave of a specified number of enemies
+    def newWave(self, numEnemies, speed, health, enemyImage):
+        
+        for i in range(numEnemies):
+            enemyList.append(newEnemy(self.TILEMAP.find_start(), self.SURF, speed, health, self.TILESIZE, enemyImage))
+
+    ## checks an enemies status, and if it needs new information, updates the enemy
+    ## NOT FINISHED NEEDS WORK
+    ## NOT FINISHED NEEDS WORK
+    ## NOT FINISHED NEEDS WORK
+    def __checkEnemyStatus(self):
+        
+        for enemy in enemyList:
+            if(enemyList[enemy].checkFeed()):
+                enemyList[enemy].updateEnemy(speed)
+
+
+
+
+
+
