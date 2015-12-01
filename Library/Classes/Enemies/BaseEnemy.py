fca41798a4f811875a387588b3a45235345ea44a
Joshua Rosen <joshrosen117@yahoo.com>
2015-11-30 19:38:02 -0700

    Merge branch 'master' into dev
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    Merging David's changes to BaseEnemy.py and EnemyWave.py from the master branch into the dev

--- Library/Classes/Enemies/BaseEnemy.py
+++ Library/Classes/Enemies/BaseEnemy.py
@@ -4,24 +4,90 @@
 #       David Mirabile
 # 
 # Purpose:
-# Generates an individual enemy.
+# Holds basic information about health and displaying of an enemy.
 
 import pygame, math
+from Library.Classes.Tiles.Path import *
+from Library.Classes.Bullets.Vector import *
 from random import randint
+
+## Implementation notice:
+## This class is an individual enemy.
+## This class will be managed by the EnemyWave class
 
 
 class newEnemy:
 
-    def __init__(self, pos, surf, speed, health):
-        self.POS = pos
-        self.SURF = surf
+    def __init__(self, pos, surf, speed, size, health, tileSize, image):
+        ## position information
+        self.pos = pos
+        self.TILESIZE = tileSize
+        self.truePositionY = pos[1] * self.TILESIZE
+        self.truePositionX = pos[0] * self.TILESIZE
         self.SPEED = speed
-        self.HEALTH = health
+        self.SIZE = size
+
+        self.SURF = surf
+        self.health = health
+        self.feed = False
+
+        self.YDIRECTION = False
+        if self.SPEED[0] ==0:
+            self.YDIRECTION = True
+        self.IMAGE = image
+
+    ## Moves the enemy, then checks to see if the enemy has reached the next tile
+    ## if the enemy has reached the next tile, requests new info
+    def __moveEnemy(self):
+
+        ## changes position according to speed
+        self.truePositionX += self.SPEED[0]
+        self.truePositionY += self.SPEED[1]
+
+        ## Checks to see if enemy is within 1 tick of the next tile
+        if self.YDIRECTION:
+            if self.truePositionY % self.SPEED[1] < abs(self.SPEED[1]):
+                self.SPEED[1] = self.truePositionY % self.SPEED[1]
+                self.feed = True
+
+        else:
+            if self.truePositionX % self.SPEED[0] < abs(self.SPEED[0]):
+                self.SPEED[0] = self.truePositionX % self.SPEED[0]
+                self.feed = True
 
 
+    ## this function will be called by the Enemy Wave class to see if an enemy needs new information.
+    ## if it returns true, the enemy wave will feed it more info.
+    def checkFeed(self):
+        if (self.feed):
+            return True
+
+        return False
+
+    def updateEnemy(self, speed):
+        self.SPEED = speed
+        self.YDIRECTION = False
+        if self.SPEED[0] ==0:
+            self.YDIRECTION = True
     
-    def damage_enemy(self, damage):
-        self.HEALTH -= damage
+    def getPosition(self):
+        return self.pos
 
+    ## Damages the enemy.
+    def damage_enemy(self, damage):
+        self.health -= damage
+    
+    ## displays the enemy.
     def disp_enemy(self):
-        pass
+        self.__moveEnemy()
+        self.SURF.blit(IMAGE, self.__findBlitPosition())
+
+    ## determines where to blit the enemy to the screen
+    def __findBlitPosition(self):
+        xPosition = truePositionX - (self.SIZE//2)
+        yPosition = truePositionY - (self.SIZE//2)
+
+        return xPosition, yPosition
+
+
+
--- Library/Classes/Enemies/EnemyWave.py
+++ Library/Classes/Enemies/EnemyWave.py
@@ -1,7 +1,90 @@
-#EnemyWave.py
+﻿#EnemyWave.py
 # 
 # File Contributors
-#
+#       David Mirabile
 # 
 # Purpose:
-# 
+# Manage and construct waves of enemies.
+
+
+from Library.Classes.Enemies.BaseEnemy import *
+
+
+## implementation notice:
+## This class is designed to manage enemies, and generate waves of enemies.
+## This class should be used when dealing with enemies
+
+class enemyWave():
+
+    ## The init requires requires all the information for the type of enemy it will be displaying
+    ## as well as how many enemies it will display, and how often to launch a new enemy.
+    ## speed must be an integer.
+    def __init__(self, tileMap, surf, speed, size, health, numEnemies, deployDelay, enemyImage):
+        self.enemyList = []
+        self.TILEMAP = tileMap
+        self.TILESIZE = self.TILEMAP.tile_size
+        self.IMAGE = enemyImage
+        self.SURF = surf
+        self.SPEED = speed
+        self.SIZE = size
+        self.HEALTH = health
+        ##self.__newWave(numEnemies)
+        self.NUM_ENEMIES = numEnemies
+
+        self.DEPLOYDELAY = deployDelay
+        self.count = 0
+        self.deployed_enemies = 0
+
+    ## passes the enemy list to a function that requests it
+    def getEnemyList(self):
+        return self.enemyList
+
+    ### Obsolete
+    ## generates a new wave of a specified number of enemies
+    ## def __newWave(self):
+    ##    
+    ##     for i in range(numEnemies):
+    ##         self.enemyList.append(newEnemy(self.TILEMAP.start_position, self.SURF, self.SPEED, self.SIZE, self.health, self.TILESIZE, self.IMAGE))
+
+    ## checks an enemies status, and if it needs new information, updates the enemy
+    def __checkEnemyStatus(self):
+        
+        for enemy in range(len(self.enemyList)):
+            if(self.enemyList[enemy].checkFeed()):
+                tempPos = enemyList[enemy].getPosition()
+                if self.TILEMAP.tile_map[tempPos[0]][tempPos[1]].current_direction == 'U':
+                    speed = (0, -self.SPEED)
+                elif self.TILEMAP.tile_map[tempPos[0]][tempPos[1]].current_direction == 'D':
+                    speed = (0, self.SPEED)
+                elif self.TILEMAP.tile_map[tempPos[0]][tempPos[1]].current_direction == 'L':
+                    speed = (-self.SPEED, 0)
+                else:
+                    speed = (self.SPEED, 0)
+                self.enemyList[enemy].updateEnemy(speed)
+
+    ## kills enemies that have no health
+    def __cullEnemies(self):
+        for enemy in range(len(self.enemyList)):
+            if self.enemyList[enemy].health <= 0:
+                del self.enemyList[enemy]
+                enemy -= 1
+
+
+    ## one call to tick all enemies forward one more frame.
+    def tick(self, enemiesToStart):
+        self.count += 1
+        if (self.count % self.DEPLOYDELAY == 0) and (self.deployed_enemies <= self.NUM_ENEMIES):
+            self.enemyList.append(newEnemy(self.TILEMAP.start_position, self.SURF, self.SPEED, self.SIZE, self.health, self.TILESIZE, self.IMAGE))
+            self.deployed_enemies += 1
+        
+        self.__cullEnemies()
+        for enemy in range(len(self.enemyList)):
+            self.enemyList[enemy].disp_enemy()
+        self.__checkEnemyStatus()
+
+
+
+
+
+
+
